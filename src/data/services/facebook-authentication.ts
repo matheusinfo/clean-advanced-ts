71cc137d6cde02e55e6df7a06a3da91734638800
Matheus Andrade <matheus.add12@gmail.com>
2023-05-01 23:50:33 -0300

    feat: ensure Service calls LoadUserAccountRepository with correct params

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,14 +1,23 @@
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 import { LoadFacebookUserApi } from '@/data/contracts/api'
+import { LoadUserAccountRepository } from '../contracts/repository'
 
 export class FacebookAuthenticationService {
   constructor (
-    private readonly loadFacebookUserApi: LoadFacebookUserApi
+    private readonly loadFacebookUserApi: LoadFacebookUserApi,
+    private readonly loadUserAccountRepository: LoadUserAccountRepository
   ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<FacebookAuthentication.Result> {
-    await this.loadFacebookUserApi.loadUser({ token: params.token })
+    const facebookApiResponse = await this.loadFacebookUserApi.loadUser({ token: params.token })
+
+    if (facebookApiResponse == null) {
+      return new AuthenticationError()
+    }
+
+    await this.loadUserAccountRepository.load({ email: facebookApiResponse.email })
+
     return new AuthenticationError()
   }
 }
