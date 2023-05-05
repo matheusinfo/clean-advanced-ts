b602f3f7dbe635aac48d8282eed220c0b2f215c3
Matheus Andrade <matheus.add12@gmail.com>
2023-05-04 21:23:07 -0300

    feat: ensure JwtTokenGenerator rethrows if sign throws

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -48,5 +48,16 @@ describe('JwtTokenGenerator', () => {
 
       expect(token).toBe('any_token')
     })
+
+    it('should rethrow if sign throws', async () => {
+      fakeJwt.sign.mockImplementationOnce(() => { throw new Error('http_error') })
+
+      const promise = sut.generateToken({
+        key: 'any_key',
+        expirationInMs: 1000
+      })
+
+      await expect(promise).rejects.toThrow(new Error('http_error'))
+    })
   })
 })
