5052fd0be724e8c50c5667df6b25f43c685d8601
Matheus Andrade <matheus.add12@gmail.com>
2023-05-04 23:03:18 -0300

    feat: ensure AxiosHttpClient calls axios.get with correct value

---
+++ tests/infra/http/axios-client.spec.ts
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import { HttpGetClient } from '@/infra/http'
+
+jest.mock('axios')
+
+class AxiosHttpClient {
+  async get ({ url, params }: HttpGetClient.Params): Promise<void> {
+    await axios.get(url, {
+      params
+    })
+  }
+}
+
+describe('AxiosHttpClient', () => {
+  describe('get', () => {
+    it('should call get with correct params', async () => {
+      const fakeAxios = axios as jest.Mocked<typeof axios>
+      const sut = new AxiosHttpClient()
+
+      await sut.get({
+        url: 'any_url',
+        params: {
+          any: 'any'
+        }
+      })
+
+      expect(fakeAxios.get).toHaveBeenCalledWith('any_url', {
+        params: {
+          any: 'any'
+        }
+      })
+      expect(fakeAxios.get).toHaveBeenCalledTimes(1)
+    })
+  })
+})
