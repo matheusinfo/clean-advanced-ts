589a925385fb95bb34c0bcd7c927790f933045bb
Matheus Andrade <matheus.add12@gmail.com>
2023-05-06 23:24:59 -0300

    refactor: move code to separate file

--- tests/infra/postgres/repository/user-account.spec.ts
+++ tests/infra/postgres/repository/user-account.spec.ts
@@ -1,17 +1,8 @@
-import { IBackup, IMemoryDb, newDb } from 'pg-mem'
+import { IBackup } from 'pg-mem'
 import { getRepository, getConnection, Repository } from 'typeorm'
 import { PostgresUser } from '@/infra/postgres/entities'
 import { PostgresUserAccountRepository } from '@/infra/postgres/repository'
-
-const makeFakeDb = async (entities?: any[]): Promise<IMemoryDb> => {
-  const db = newDb()
-  const connection = await db.adapters.createTypeormConnection({
-    type: 'postgres',
-    entities: entities ?? 'src/infra/postgres/entities/index.ts'
-  })
-  await connection.synchronize()
-  return db
-}
+import { makeFakeDb } from '@/tests/infra/postgres/mocks'
 
 describe('PostgresUserAccountRepository', () => {
   describe('load', () => {
