2463f8ad9dd39807f1ce44e6d7e3b6a64b2f97a2
Matheus Andrade <matheus.add12@gmail.com>
2023-05-06 23:42:17 -0300

    feat: add implementation to infra layer

--- src/infra/http/axios-client.ts
+++ src/infra/http/axios-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { HttpGetClient } from './client'
 
-export class AxiosHttpClient {
+export class AxiosHttpClient implements HttpGetClient {
   async get ({ url, params }: HttpGetClient.Params): Promise<any> {
     const result = await axios.get(url, {
       params
