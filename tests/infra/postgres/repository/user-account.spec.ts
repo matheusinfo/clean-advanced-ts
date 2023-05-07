2f5c5c36b5043441a9f5801c804c78764a269719
Matheus Andrade <matheus.add12@gmail.com>
2023-05-06 23:26:27 -0300

    refactor: share sut creation

--- tests/infra/postgres/repository/user-account.spec.ts
+++ tests/infra/postgres/repository/user-account.spec.ts
@@ -5,42 +5,36 @@ import { PostgresUserAccountRepository } from '@/infra/postgres/repository'
 import { makeFakeDb } from '@/tests/infra/postgres/mocks'
 
 describe('PostgresUserAccountRepository', () => {
+  let sut: PostgresUserAccountRepository
+  let postgresUserRepository: Repository<PostgresUser>
+  let dbBackup: IBackup
+
+  beforeAll(async () => {
+    const db = await makeFakeDb([PostgresUser])
+    dbBackup = db.backup()
+    postgresUserRepository = getRepository(PostgresUser)
+  })
+
+  beforeEach(() => {
+    dbBackup.restore()
+    sut = new PostgresUserAccountRepository()
+  })
+
+  afterAll(async () => {
+    await getConnection().close()
+  })
+
   describe('load', () => {
-    let sut: PostgresUserAccountRepository
-    let postgresUserRepository: Repository<PostgresUser>
-    let dbBackup: IBackup
-
-    beforeAll(async () => {
-      const db = await makeFakeDb([PostgresUser])
-      dbBackup = db.backup()
-      postgresUserRepository = getRepository(PostgresUser)
-    })
-
-    beforeEach(() => {
-      dbBackup.restore()
-      sut = new PostgresUserAccountRepository()
-    })
-
-    afterAll(async () => {
-      await getConnection().close()
-    })
-
     it('should return an account if email exists', async () => {
       await postgresUserRepository.save({ email: 'existing_email' })
 
-      const account = await sut.load({
-        email: 'existing_email'
-      })
+      const account = await sut.load({ email: 'existing_email' })
 
-      expect(account).toEqual({
-        id: '1'
-      })
+      expect(account).toEqual({ id: '1' })
     })
 
     it('should return undefined if email does not exists', async () => {
-      const account = await sut.load({
-        email: 'existing_email'
-      })
+      const account = await sut.load({ email: 'existing_email' })
 
       expect(account).toBeUndefined()
     })
