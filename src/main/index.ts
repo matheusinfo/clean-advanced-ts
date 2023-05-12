2a4c2b9a6e01d0d27453cba945cd2d7381845857
Matheus Andrade <matheus.add12@gmail.com>
2023-05-11 23:53:02 -0300

    feat: configure express server

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,6 @@
+import './config/module-alias'
 import 'reflect-metadata'
+import { app } from './config/app'
+import { env } from './config/env'
+
+app.listen(env.app.port, () => console.log(`Server running at http://localhost:${env.app.port}`))
