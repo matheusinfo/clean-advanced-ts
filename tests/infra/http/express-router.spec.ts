4689f29c1b5bce7e76289291aabc55df234cbdfe
Matheus Andrade <matheus.add12@gmail.com>
2023-05-11 23:18:24 -0300

    refactor: ensure ExpressRouter responds with 200 and valid data

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -7,9 +7,10 @@ export class ExpressRouter {
   constructor (private readonly controller: Controller) {}
 
   async adapt (req: Request, res: Response): Promise<void> {
-    await this.controller.handle({
+    const httpResponse = await this.controller.handle({
       ...req.body
     })
+    res.status(200).json(httpResponse.data)
   }
 }
 
@@ -23,12 +24,17 @@ describe('ExpressRouter', () => {
     req = getMockReq({ body: { any: 'any' } })
     res = getMockRes().res
     controller = mock()
+    controller.handle.mockResolvedValue({
+      statusCode: 200,
+      data: { data: 'any_data' }
+    })
     sut = new ExpressRouter(controller)
   })
 
   it('should call handle with correct request', async () => {
     await sut.adapt(req, res)
 
+    expect(controller.handle).toHaveBeenCalledTimes(1)
     expect(controller.handle).toHaveBeenCalledWith({
       any: 'any'
     })
@@ -39,6 +45,16 @@ describe('ExpressRouter', () => {
 
     await sut.adapt(req, res)
 
+    expect(controller.handle).toHaveBeenCalledTimes(1)
     expect(controller.handle).toHaveBeenCalledWith({})
+  })
+
+  it('should respond with 200 and valid data', async () => {
+    await sut.adapt(req, res)
+
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(res.json).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({ data: 'any_data' })
   })
 })
