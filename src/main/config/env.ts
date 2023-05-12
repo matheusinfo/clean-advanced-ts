1246bfa284915e2c6c6a3076907c859c4dd526d1
Matheus Andrade <matheus.add12@gmail.com>
2023-05-11 21:53:02 -0300

    feat: configure express server

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,8 @@ export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? '',
     clientSecret: process.env.FB_CLIENT_SECRET ?? ''
+  },
+  app: {
+    port: process.env.PORT ?? 8080
   }
 }
