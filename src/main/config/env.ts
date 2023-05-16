64f497e15ead9273a6d147569b2799b032adebaa
Matheus Andrade <matheus.add12@gmail.com>
2023-05-15 23:10:36 -0300

    connect to postgres db before server init

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -6,5 +6,14 @@ export const env = {
   app: {
     port: process.env.PORT ?? 8080,
     jwtSecret: process.env.JWT_SECRET ?? 'cahuh#@@'
+  },
+  db: {
+    postgresMain: {
+      host: process.env.POSTGRES_HOST ?? 'localhost',
+      port: process.env.POSTGRES_PORT ?? 5432,
+      username: process.env.POSTGRES_USERNAME ?? 'postgres',
+      password: process.env.POSTGRES_PASSWORD ?? 'postgres',
+      database: process.env.POSTGRES_DATABASE ?? 'postgres'
+    }
   }
 }
