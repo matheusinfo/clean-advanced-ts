71fa16e66cfb9622bd2acc236b937bbd059160cb
Matheus Andrade <matheus.add12@gmail.com>
2023-05-17 23:12:42 -0300

    refactor: change express router

--- src/infra/http/express-router.ts
+++ src/infra/http/express-router.ts
@@ -1,15 +1,15 @@
 import { RequestHandler } from 'express'
 import { Controller } from '@/application/controllers'
 
-export const adaptExpressRoute = (controller: Controller): RequestHandler => {
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  return async (req, res) => {
-    const { data, statusCode } = await controller.handle({ ...req.body })
+type Adapter = (controller: Controller) => RequestHandler
 
-    if (statusCode >= 200 && statusCode <= 299) {
-      res.status(statusCode).json(data)
-    } else {
-      res.status(statusCode).json({ error: data.message })
-    }
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+export const adaptExpressRoute: Adapter = controller => async (req, res) => {
+  const { data, statusCode } = await controller.handle({ ...req.body })
+
+  if (statusCode >= 200 && statusCode <= 299) {
+    res.status(statusCode).json(data)
+  } else {
+    res.status(statusCode).json({ error: data.message })
   }
 }
